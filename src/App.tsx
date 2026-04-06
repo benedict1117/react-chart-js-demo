c3bddc993b5ec1a5247637492bb986701f86b85a
khianvictorycalderon <ckhianvictory@gmail.com>
2026-04-06 15:44:13 +0800

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+      <div className="text-center">
+        <h1 className="text-2xl font-bold">React ChartJS Demo</h1>
+      </div>
+    </div>
+  )
+}
